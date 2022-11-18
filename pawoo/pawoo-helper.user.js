f5c5191c8a629d87a7203cadfcd4f9f3be499f20
Nc5xb3 <nc5xb3@gmail.com>
2022-11-18 23:50:00 +1100

    bypass csp with tampermonkey request function

--- pawoo/pawoo-helper.user.js
+++ pawoo/pawoo-helper.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name         Nc Pawoo Helper
 // @namespace    https://pawoo.net/
-// @version      0.1
+// @version      0.2
 // @description  Beep boop
 // @author       Nc5xb3
 // @match        https://pawoo.net/*
 // @match        https://www.pawoo.net/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pawoo.net
-// @grant        none
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// @connect      script.google.com
+// @connect      script.googleusercontent.com
 // @require      https://code.jquery.com/jquery-3.6.1.min.js
 // ==/UserScript==
 
@@ -17,7 +20,10 @@
     'use strict';
 
     const gtUrl = 'https://translate.google.com.au/?sl=ja&tl=en&op=translate&text=';
+    const gtsUrl = 'https://script.google.com/macros/s/AKfycbwu1fQ7xNENwU2sa9aTWPSX4gNo6haowdMhzTQhX3eZ8c8Od5KMDe2ddQKH_cjQ3hyS/exec?source=ja&target=en&text=';
     var contentIndex = 0;
+
+    GM_addStyle('div.nc-translation.hide { display: none; }');
 
     function openTranslate(id) {
         var content = $('div[nc-content-id=' + id + ']');
@@ -30,7 +36,27 @@
                 html = html.replace(/\<br\>/g, "\n");
                 text.push($(html).text());
             })
-            window.open(gtUrl + encodeURIComponent(text.join("\n")), '_blank');
+            var message = encodeURIComponent(text.join("\n"));
+
+            var translationButton = $('button[nc-button-id="' + id + '"]');
+            var translationBlock = $('div[nc-translation-id="' + id + '"]');
+
+            translationBlock.html('translation...');
+            GM_xmlhttpRequest({
+                method: 'GET',
+                url: gtsUrl + message,
+                onload: function (res) {
+                    if (res.status === 200) {
+                        var translation = res.responseText;
+                        translationBlock.html(translation)
+                        translationButton.attr('state', 'on').html('Translated from Japanese by Google');
+                    } else {
+                        console.error('Failed to translate. Opening google translate page instead');
+                        window.open(gtUrl + message, '_blank');
+                        //window.open('https://www.deepl.com/en/translator?share=generic#ja/en/' + encodeURIComponent(text.join("\n")), '_blank');
+                    }
+                }
+            });
         } else {
             console.log('div[nc-content-id=' + id + '] not found')
         }
@@ -53,15 +79,35 @@
 
             // seems like they disabled onclick functions, so adding listeners..
             var link = $('<button/>')
-            .attr('nc-content-id', contentIndex)
-            .html('open google translate')
-
-            link.on('click', () => {
-                var id = $(this).attr('nc-content-id')
-                openTranslate(id)
+            .attr('nc-button-id', contentIndex)
+            .attr('state', 'off')
+            .html('Translate Toot')
+            .on('click', function() {
+                var id = $(this).attr('nc-button-id');
+                var state = $(this).attr('state');
+                var translationBlock = $('div[nc-translation-id="' + id + '"]');
+                var translation = translationBlock.text();
+                if (state === 'off') {
+                    translationBlock.removeClass('hide');
+                    if (translation.length > 0) {
+                        console.log('show existing translation', id);
+                        $(this).attr('state', 'on').html('Translated from Japanese by Google');
+                    } else {
+                        console.log('translate', id);
+                        openTranslate(id)
+                    }
+                } else {
+                    console.log('hide translation', id);
+                    translationBlock.addClass('hide');
+                    $(this).attr('state', 'off').html('Translate Toot');
+                }
             });
+            link.insertAfter(el);
 
-            link.insertAfter(el)
+            var tlBlock = $('<div/>')
+            .attr('nc-translation-id', contentIndex)
+            .addClass('nc-translation');
+            tlBlock.insertAfter(link);
         })
         if (contents.length) {
             console.log('found ' + contents.length + ' contents');
